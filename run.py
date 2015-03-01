2c07eace4e6e4e3dd177b3388e599949345824a7
Toby Ho <airportyh@gmail.com>
2015-03-01 02:53:23 -0500

    Refactoring.

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import xlrd
 import re
+import sys
 
 def parse_numbers(s):
   numbers = []
@@ -18,7 +19,21 @@ def parse_numbers(s):
       numbers.append(int(part))
   return numbers
 
-def all_numbers(lst):
+def cave_numbers_for_cell(cell):
+  if cell.ctype != xlrd.XL_CELL_EMPTY:
+    if cell.ctype == xlrd.XL_CELL_NUMBER:
+      numbers = "{:0.0f}".format(cell.value)
+    else:
+      numbers = cell.value
+      numbers = numbers.replace(u'\xef\xbc\x8c', u',')
+      numbers = numbers.replace(u'\uff0c', u',')
+    numbers = parse_numbers(numbers)
+    assert has_all_numbers(numbers)
+    return numbers
+  else:
+    return []
+
+def has_all_numbers(lst):
   if type(lst) != list:
     return False
   for n in lst:
@@ -26,45 +41,49 @@ def all_numbers(lst):
       return False
   return True
 
-book = xlrd.open_workbook('cloister caves.xls')
-sheet = book.sheet_by_index(0)
+if len(sys.argv) < 2:
+  print "Usage: python run.py <excel file>"
+  sys.exit()
 
-col_offset = 2
-i = 0
-locations = []
-while i < 15:
-  name = sheet.cell(0, col_offset + i).value
-  locations.append({
-    'name': name,
-    'col_idx': col_offset + i
-  })
-  i = i + 1
+def read_file(filename):
+  book = xlrd.open_workbook(filename, formatting_info=True)
+  sheet = book.sheet_by_index(0)
+  
+  col_offset = 2
+  caves = set()
+  locations = []
+  artifacts = []
+  sitings = []
 
-row_offset = 3
-artifacts = []
-i = 0
-while i < 176:
-  artifact_name = sheet.cell(row_offset + i, 0)
-  artifact_chinese_name = sheet.cell(row_offset + i, 1)
-  artifacts.append({
-    'name': artifact_name.value,
-    'chinese_name': artifact_chinese_name.value,
-    'row_idx': row_offset + i
-  })
-  i = i + 1
+  for i in xrange(15):
+    name = sheet.cell(0, col_offset + i).value
+    locations.append({
+      'id': i,
+      'name': name,
+      'col_idx': col_offset + i
+    })
 
-for artifact in artifacts:
-  for location in locations:
-    cell = sheet.cell(artifact['row_idx'], location['col_idx'])
-    if cell.ctype != xlrd.XL_CELL_EMPTY:
-      if cell.ctype == xlrd.XL_CELL_NUMBER:
-        numbers = "{:0.0f}".format(cell.value)
-      else:
-        numbers = cell.value
-        numbers = numbers.replace(u'\xef\xbc\x8c', u',')
-        numbers = numbers.replace(u'\uff0c', u',')
-      numbers = parse_numbers(numbers)
-      assert all_numbers(numbers)
-      print "These caves have a", artifact['name'], "in in the", location['name'], ":", numbers
-      
+  row_offset = 3
+  for i in xrange(176):
+    artifact_name = sheet.cell(row_offset + i, 0)
+    artifact_chinese_name = sheet.cell(row_offset + i, 1)
+    artifacts.append({
+      'id': i,
+      'name': artifact_name.value,
+      'chinese_name': artifact_chinese_name.value,
+      'row_idx': row_offset + i
+    })
+
+  for artifact in artifacts:
+    for location in locations:
+      cell = sheet.cell(artifact['row_idx'], location['col_idx'])
+      cave_numbers = cave_numbers_for_cell(cell)
+      for cave in cave_numbers:
+        caves.add(cave)
+        
+  print caves
+
+filename = sys.argv[1]
+print "Opening", filename
+read_file(filename)
       
