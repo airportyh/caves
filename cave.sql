8397b00f08500493c743c40fff80c584ad06a69d
Toby Ho <airportyh@gmail.com>
2015-03-06 15:55:50 -0500

    Renamed stuff.

--- cave.sql
+++ cave.sql
@@ -1,22 +1,36 @@
-CREATE TABLE cave (id INTEGER PRIMARY KEY,
-		name TEXT,
-		number INTEGER);
+CREATE TABLE cave (
+  id SERIAL PRIMARY KEY,
+  type_id INTEGER, -- moved siting type_id to cave_type_id
+  name TEXT,
+  number INTEGER);
 
-CREATE TABLE type (id INTEGER PRIMARY KEY,
-		type_name TEXT);
-		
-CREATE TABLE icon (
-  id INTEGER PRIMARY KEY,
-	name TEXT,
+CREATE TABLE cave_type ( -- renamed type to cave_type
+  id SERIAL PRIMARY KEY,
+  name TEXT);
+
+CREATE TABLE architecture (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  chinese_name TEXT
+);
+  
+CREATE TABLE iconography (
+  id SERIAL PRIMARY KEY,
+  type_id INTEGER,
+  name TEXT,
   chinese_name TEXT
 );
 
-CREATE TABLE architecture (
-  id INTEGER PRIMARY KEY,
-	location_description TEXT,
-  chinese_location_description TEXT
+CREATE TABLE iconography_type ( -- added icon type for "Architectural Elements", "Inscriptions", "Ceiling types", etc
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  chinese_name TEXT
 );
 
-CREATE TABLE siting (cave_id INTEGER, 
-			type_id INTEGER, icon_id INTEGER, 
-			architecture_id INTEGER);
+CREATE TABLE siting (
+  id SERIAL PRIMARY KEY,  -- added id for siting (SERIAL is an INTEGER file tha automatically increments)
+  cave_id INTEGER, 
+  iconography_id INTEGER, 
+              -- moved type_if from here to cave.cave_type_id
+  architecture_id INTEGER
+);
--- load_db.sh
+++ load_db.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 python run.py
-sqlite3 caves.sqlite < cave.toby.sql
-sqlite3 caves.sqlite < data.sql
+sqlite3 cave.sqlite < cave.sql
+sqlite3 cave.sqlite < data.sql
